a0f824f953ffcd1c0be5c5464060fb63c5b6db83
Shatel <qosay.alshatel11@yahoo.com>
2022-01-15 17:45:34 +0200

    Create account done

--- POS_System_Arthurs/CreateAccount.Designer.cs
+++ POS_System_Arthurs/CreateAccount.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.signInButton = new System.Windows.Forms.Button();
             this.userNameTextbox = new System.Windows.Forms.TextBox();
             this.passwordTextbox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -52,53 +52,26 @@
             this.panel1.Size = new System.Drawing.Size(401, 452);
             this.panel1.TabIndex = 0;
             // 
-            // button1
+            // label3
             // 
-            this.button1.Location = new System.Drawing.Point(585, 234);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(366, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "It should only take a few seconds to connect";
             // 
-            // button2
+            // pictureBox1
             // 
-            this.button2.Location = new System.Drawing.Point(468, 234);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // userNameTextbox
-            // 
-            this.userNameTextbox.Location = new System.Drawing.Point(468, 122);
-            this.userNameTextbox.Name = "userNameTextbox";
-            this.userNameTextbox.Size = new System.Drawing.Size(129, 20);
-            this.userNameTextbox.TabIndex = 3;
-            this.userNameTextbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.userNameTextbox_MouseClick);
-            this.userNameTextbox.Leave += new System.EventHandler(this.userNameTextbox_Leave);
-            // 
-            // passwordTextbox
-            // 
-            this.passwordTextbox.Location = new System.Drawing.Point(468, 177);
-            this.passwordTextbox.Name = "passwordTextbox";
-            this.passwordTextbox.Size = new System.Drawing.Size(129, 20);
-            this.passwordTextbox.TabIndex = 4;
-            this.passwordTextbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.passwordTextbox_MouseClick);
-            this.passwordTextbox.Leave += new System.EventHandler(this.passwordTextbox_Leave);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 20F);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(468, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 35);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Create new account";
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(117)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Image = global::POS_System_Arthurs.Properties.Resources.newUserIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(97, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 197);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
@@ -111,26 +84,69 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Let\'s get you set up";
             // 
-            // pictureBox1
+            // signUpButton
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(117)))), ((int)(((byte)(128)))));
-            this.pictureBox1.Image = global::POS_System_Arthurs.Properties.Resources.newUserIcon;
-            this.pictureBox1.Location = new System.Drawing.Point(97, 60);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(195, 197);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.signUpButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.signUpButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(117)))), ((int)(((byte)(128)))));
+            this.signUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signUpButton.ForeColor = System.Drawing.Color.White;
+            this.signUpButton.Location = new System.Drawing.Point(443, 265);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(2);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(140, 23);
+            this.signUpButton.TabIndex = 1;
+            this.signUpButton.Text = "Sign up";
+            this.signUpButton.UseVisualStyleBackColor = false;
+            this.signUpButton.Click += new System.EventHandler(this.signUpButton_Click);
             // 
-            // label3
+            // signInButton
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Tahoma", 13F);
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(12, 363);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(366, 22);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "It should only take a few seconds to connect";
+            this.signInButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.signInButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(117)))), ((int)(((byte)(128)))));
+            this.signInButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signInButton.ForeColor = System.Drawing.Color.White;
+            this.signInButton.Location = new System.Drawing.Point(621, 265);
+            this.signInButton.Margin = new System.Windows.Forms.Padding(2);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(140, 23);
+            this.signInButton.TabIndex = 2;
+            this.signInButton.Text = "Sign in";
+            this.signInButton.UseVisualStyleBackColor = false;
+            this.signInButton.Click += new System.EventHandler(this.signInButton_Click);
+            // 
+            // userNameTextbox
+            // 
+            this.userNameTextbox.Location = new System.Drawing.Point(468, 125);
+            this.userNameTextbox.Name = "userNameTextbox";
+            this.userNameTextbox.Size = new System.Drawing.Size(174, 20);
+            this.userNameTextbox.TabIndex = 3;
+            this.userNameTextbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.userNameTextbox_MouseClick);
+            this.userNameTextbox.Leave += new System.EventHandler(this.userNameTextbox_Leave);
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(468, 188);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '*';
+            this.passwordTextbox.Size = new System.Drawing.Size(174, 20);
+            this.passwordTextbox.TabIndex = 4;
+            this.passwordTextbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.passwordTextbox_MouseClick);
+            this.passwordTextbox.Leave += new System.EventHandler(this.passwordTextbox_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(443, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 43);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Create new account";
             // 
             // CreateAccount
             // 
@@ -141,8 +157,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.passwordTextbox);
             this.Controls.Add(this.userNameTextbox);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.signInButton);
+            this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.panel1);
             this.Name = "CreateAccount";
             this.Text = "CreateAccount";
@@ -158,8 +174,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.Button signInButton;
         private System.Windows.Forms.TextBox userNameTextbox;
         private System.Windows.Forms.TextBox passwordTextbox;
         private System.Windows.Forms.Label label1;
